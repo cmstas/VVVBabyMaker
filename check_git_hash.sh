ca17d1ad0abe199ee28092c129f577a266d9ccd1
Philip Chang <sgnoohc@gmail.com>
2019-02-22 15:07:48 -0800

    Added a way to check hashes easily

---
+++ check_git_hash.sh
@@ -0,0 +1,18 @@
+echo "wwwbabymaker hash"
+git rev-parse HEAD
+cd CORE
+echo "CORE hash"
+git rev-parse HEAD
+cd ../
+cd dilepbabymaker/rooutil
+echo "rooutil hash"
+git rev-parse HEAD
+cd ../
+cd coreutil
+echo "coreutil hash"
+git rev-parse HEAD
+cd ../
+cd ProjectMetis
+echo "ProjectMetis hash"
+git rev-parse HEAD
+cd ../../../
