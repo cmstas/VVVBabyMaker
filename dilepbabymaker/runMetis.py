8156a7cf6d46de10b210508c38b34767e980460d
Philip Chang <sgnoohc@gmail.com>
2019-03-15 11:09:47 -0700

    Apparently we don't have CMS4 for ext1 ZZTo4L....

--- dilepbabymaker/runMetis.py
+++ dilepbabymaker/runMetis.py
@@ -412,7 +412,7 @@ def get_master_list():
         # WZ 3LNu 3
         # WZ 3LNu 3
         # WZ 3LNu powheg
-        "/ZZTo4L_TuneCP5_13TeV_powheg_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2/MINIAODSIM"                                 : "zz_4l_powheg"               , 
+        "/ZZTo4L_TuneCP5_13TeV_powheg_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext2-v2/MINIAODSIM"                                 : "zz_4l_powheg"               , 
         "/WWZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2/MINIAODSIM"                                  : "wwz_amcatnlo"               , 
         "/WZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15_ext1-v2/MINIAODSIM"                                  : "wzz_amcatnlo"               , 
         "/WZG_TuneCP5_13TeV-amcatnlo-pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v1/MINIAODSIM"                                       : "wzg_amcatnlo"               , 
