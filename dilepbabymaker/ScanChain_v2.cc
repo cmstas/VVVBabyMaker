e2831fcf02a64c006e8ac753b16b3af9af3cc07b
Philip Chang <sgnoohc@gmail.com>
2019-03-20 16:32:29 -0700

    Updated dilepton trigger list for 2018 and changed the filters. and also double checked gam+X sample vetophotonprocess flag

--- dilepbabymaker/ScanChain_v2.cc
+++ dilepbabymaker/ScanChain_v2.cc
@@ -438,6 +438,7 @@ void babyMaker_v2::AddWWWBabyOutput()
     tx->createBranch<int>("ngenLepFromTau");
     tx->createBranch<int>("ngenLepFromBoson");
 
+    tx->createBranch<int>("passesMETfiltersRun2");
     tx->createBranch<int>("Flag_AllEventFilters");
     tx->createBranch<int>("Flag_EcalDeadCellTriggerPrimitiveFilter");
     tx->createBranch<int>("Flag_HBHEIsoNoiseFilter");
@@ -2872,20 +2873,61 @@ void babyMaker_v2::FillTrigger()
 {
     if (cms3.evt_isRealData())
     {
-        tx->setBranch<int>("HLT_DoubleMu", coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_DoubleMu_2017 : coreTrigger.HLT_DoubleMu);
-        tx->setBranch<int>("HLT_DoubleEl", coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_DoubleEl_2017 : coreTrigger.HLT_DoubleEl);
-        tx->setBranch<int>("HLT_DoubleEl_DZ", coreTrigger.HLT_DoubleEl_DZ);
-        tx->setBranch<int>("HLT_DoubleEl_DZ_2", coreTrigger.HLT_DoubleEl_DZ_2);
-        tx->setBranch<int>("HLT_MuEG", coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_MuEG_2017 : coreTrigger.HLT_MuEG);
-        tx->setBranch<int>("HLT_MuEG_2016", coreTrigger.HLT_MuEG);
-        tx->setBranch<int>("HLT_SingleEl8"  , coreTrigger.HLT_SingleEl8_2017 );
-        tx->setBranch<int>("HLT_SingleEl17" , coreTrigger.HLT_SingleEl17_2017 );
-        tx->setBranch<int>("HLT_SingleIsoEl8"  , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoEl8_2017  : coreTrigger.HLT_SingleIsoEl8  );
-        tx->setBranch<int>("HLT_SingleIsoEl17" , coreTrigger.HLT_SingleIsoEl17 );
-        tx->setBranch<int>("HLT_SingleIsoEl23" , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoEl23_2017 : coreTrigger.HLT_SingleIsoEl23 );
-        tx->setBranch<int>("HLT_SingleIsoMu8"  , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoMu8_2017  : coreTrigger.HLT_SingleIsoMu8  );
-        tx->setBranch<int>("HLT_SingleIsoMu17" , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoMu17_2017 : coreTrigger.HLT_SingleIsoMu17 );
-        tx->setBranch<int>("HLT_PFMET140_PFMHT140_IDTight", coreTrigger.HLT_PFMET140_PFMHT140_IDTight);
+
+        if (gconf.year == 2016)
+        {
+            tx->setBranch<int>("HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu                  );
+            tx->setBranch<int>("HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl                  );
+            tx->setBranch<int>("HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
+            tx->setBranch<int>("HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
+            tx->setBranch<int>("HLT_MuEG"                      , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2017            );
+            tx->setBranch<int>("HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2017           );
+            tx->setBranch<int>("HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8              );
+            tx->setBranch<int>("HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
+            tx->setBranch<int>("HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23             );
+            tx->setBranch<int>("HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8              );
+            tx->setBranch<int>("HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17             );
+            tx->setBranch<int>("HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
+        }
+
+        else if (gconf.year == 2017)
+        {
+            tx->setBranch<int>("HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu_2017             );
+            tx->setBranch<int>("HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl_2017             );
+            tx->setBranch<int>("HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
+            tx->setBranch<int>("HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
+            tx->setBranch<int>("HLT_MuEG"                      , coreTrigger.HLT_MuEG_2017                 );
+            tx->setBranch<int>("HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2017            );
+            tx->setBranch<int>("HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2017           );
+            tx->setBranch<int>("HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8_2017         );
+            tx->setBranch<int>("HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
+            tx->setBranch<int>("HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23_2017        );
+            tx->setBranch<int>("HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8_2017         );
+            tx->setBranch<int>("HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17_2017        );
+            tx->setBranch<int>("HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
+        }
+
+        else if (gconf.year == 2018)
+        {
+            tx->setBranch<int>("HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu_2018             );
+            tx->setBranch<int>("HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl_2018             );
+            tx->setBranch<int>("HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
+            tx->setBranch<int>("HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
+            tx->setBranch<int>("HLT_MuEG"                      , coreTrigger.HLT_MuEG_2018                 );
+            tx->setBranch<int>("HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2018            );
+            tx->setBranch<int>("HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2018           );
+            tx->setBranch<int>("HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8_2018         );
+            tx->setBranch<int>("HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
+            tx->setBranch<int>("HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23_2018        );
+            tx->setBranch<int>("HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8_2018         );
+            tx->setBranch<int>("HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17_2018        );
+            tx->setBranch<int>("HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
+        }
+
     }
     else
     {
@@ -2919,28 +2961,88 @@ void babyMaker_v2::FillTrigger()
     }
     else
     {
-        tx->setBranch<int>("mc_HLT_DoubleMu", coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_DoubleMu_2017 : coreTrigger.HLT_DoubleMu);
-        tx->setBranch<int>("mc_HLT_DoubleEl", coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_DoubleEl_2017 : coreTrigger.HLT_DoubleEl);
-        tx->setBranch<int>("mc_HLT_DoubleEl", coreTrigger.HLT_DoubleEl);
-        tx->setBranch<int>("mc_HLT_DoubleEl_DZ", coreTrigger.HLT_DoubleEl_DZ);
-        tx->setBranch<int>("mc_HLT_DoubleEl_DZ_2", coreTrigger.HLT_DoubleEl_DZ_2);
-        tx->setBranch<int>("mc_HLT_MuEG", coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_MuEG_2017 : coreTrigger.HLT_MuEG);
-        tx->setBranch<int>("mc_HLT_MuEG_2016", coreTrigger.HLT_MuEG);
-        tx->setBranch<int>("mc_HLT_SingleEl8"  , coreTrigger.HLT_SingleEl8_2017 );
-        tx->setBranch<int>("mc_HLT_SingleEl17" , coreTrigger.HLT_SingleEl17_2017 );
-        tx->setBranch<int>("mc_HLT_SingleIsoEl8"  , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoEl8_2017  : coreTrigger.HLT_SingleIsoEl8  );
-        tx->setBranch<int>("mc_HLT_SingleIsoEl17" , coreTrigger.HLT_SingleIsoEl17 );
-        tx->setBranch<int>("mc_HLT_SingleIsoEl23" , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoEl23_2017 : coreTrigger.HLT_SingleIsoEl23 );
-        tx->setBranch<int>("mc_HLT_SingleIsoMu8"  , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoMu8_2017  : coreTrigger.HLT_SingleIsoMu8  );
-        tx->setBranch<int>("mc_HLT_SingleIsoMu17" , coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_SingleIsoMu17_2017 : coreTrigger.HLT_SingleIsoMu17 );
-        tx->setBranch<int>("mc_HLT_PFMET140_PFMHT140_IDTight", coreTrigger.HLT_PFMET140_PFMHT140_IDTight);
+
+        if (gconf.year == 2016)
+        {
+            tx->setBranch<int>("mc_HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu                  );
+            tx->setBranch<int>("mc_HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl                  );
+            tx->setBranch<int>("mc_HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
+            tx->setBranch<int>("mc_HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
+            tx->setBranch<int>("mc_HLT_MuEG"                      , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("mc_HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("mc_HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2017            );
+            tx->setBranch<int>("mc_HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2017           );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8              );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23             );
+            tx->setBranch<int>("mc_HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8              );
+            tx->setBranch<int>("mc_HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17             );
+            tx->setBranch<int>("mc_HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
+        }
+
+        else if (gconf.year == 2017)
+        {
+            tx->setBranch<int>("mc_HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu_2017             );
+            tx->setBranch<int>("mc_HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl_2017             );
+            tx->setBranch<int>("mc_HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
+            tx->setBranch<int>("mc_HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
+            tx->setBranch<int>("mc_HLT_MuEG"                      , coreTrigger.HLT_MuEG_2017                 );
+            tx->setBranch<int>("mc_HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("mc_HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2017            );
+            tx->setBranch<int>("mc_HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2017           );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8_2017         );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23_2017        );
+            tx->setBranch<int>("mc_HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8_2017         );
+            tx->setBranch<int>("mc_HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17_2017        );
+            tx->setBranch<int>("mc_HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
+        }
+
+        else if (gconf.year == 2018)
+        {
+            tx->setBranch<int>("mc_HLT_DoubleMu"                  , coreTrigger.HLT_DoubleMu_2018             );
+            tx->setBranch<int>("mc_HLT_DoubleEl"                  , coreTrigger.HLT_DoubleEl_2018             );
+            tx->setBranch<int>("mc_HLT_DoubleEl_DZ"               , coreTrigger.HLT_DoubleEl_DZ               );
+            tx->setBranch<int>("mc_HLT_DoubleEl_DZ_2"             , coreTrigger.HLT_DoubleEl_DZ_2             );
+            tx->setBranch<int>("mc_HLT_MuEG"                      , coreTrigger.HLT_MuEG_2018                 );
+            tx->setBranch<int>("mc_HLT_MuEG_2016"                 , coreTrigger.HLT_MuEG                      );
+            tx->setBranch<int>("mc_HLT_SingleEl8"                 , coreTrigger.HLT_SingleEl8_2018            );
+            tx->setBranch<int>("mc_HLT_SingleEl17"                , coreTrigger.HLT_SingleEl17_2018           );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl8"              , coreTrigger.HLT_SingleIsoEl8_2018         );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl17"             , coreTrigger.HLT_SingleIsoEl17             );
+            tx->setBranch<int>("mc_HLT_SingleIsoEl23"             , coreTrigger.HLT_SingleIsoEl23_2018        );
+            tx->setBranch<int>("mc_HLT_SingleIsoMu8"              , coreTrigger.HLT_SingleIsoMu8_2018         );
+            tx->setBranch<int>("mc_HLT_SingleIsoMu17"             , coreTrigger.HLT_SingleIsoMu17_2018        );
+            tx->setBranch<int>("mc_HLT_PFMET140_PFMHT140_IDTight" , coreTrigger.HLT_PFMET140_PFMHT140_IDTight );
+        }
+
     }
     if (cms3.evt_isRealData())
     {
-        bool trig_ee = coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_DoubleEl_2017 : coreTrigger.HLT_DoubleEl || coreTrigger.HLT_DoubleEl_DZ;
-        bool trig_em = coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_MuEG_2017     : coreTrigger.HLT_MuEG;
-        bool trig_mm = coreSample.is2017(looper.getCurrentFileName()) ? coreTrigger.HLT_DoubleMu_2017 : coreTrigger.HLT_DoubleMu;
-        bool is_pd_ee = looper.getCurrentFileName().Contains("DoubleEG");
+        bool trig_ee = true;
+        bool trig_em = true;
+        bool trig_mm = true;
+
+        if (gconf.year == 2016)
+        {
+            trig_ee = coreTrigger.HLT_DoubleEl || coreTrigger.HLT_DoubleEl_DZ;
+            trig_em = coreTrigger.HLT_MuEG;
+            trig_mm = coreTrigger.HLT_DoubleMu;
+        }
+        else if (gconf.year == 2017)
+        {
+            trig_ee = coreTrigger.HLT_DoubleEl_2017;
+            trig_em = coreTrigger.HLT_MuEG_2017;
+            trig_mm = coreTrigger.HLT_DoubleMu_2017;
+        }
+        else if (gconf.year == 2018)
+        {
+            trig_ee = coreTrigger.HLT_DoubleEl_2018;
+            trig_em = coreTrigger.HLT_MuEG_2018;
+            trig_mm = coreTrigger.HLT_DoubleMu_2018;
+        }
+
+        bool is_pd_ee = (looper.getCurrentFileName().Contains("DoubleEG") || looper.getCurrentFileName().Contains("EGamma"));
         bool is_pd_em = looper.getCurrentFileName().Contains("MuonEG");
         bool is_pd_mm = looper.getCurrentFileName().Contains("DoubleMuon");
         bool pass_duplicate_ee_em_mm = false;
@@ -3266,6 +3368,8 @@ void babyMaker_v2::FillMETFilter()
         }
         tx->setBranch<int>("fastsimfilt", fastsimfilt);
     }
+
+    tx->setBranch<int>("passesMETfiltersRun2", passesMETfiltersRun2(cms3.evt_isRealData()));
 }
 
 //##############################################################################################################
@@ -6378,6 +6482,7 @@ bool babyMaker_v2::vetophotonprocess()
        ||looper.getCurrentFileName().Contains("/DYJets")
        ||looper.getCurrentFileName().Contains("/TT_")
        ||looper.getCurrentFileName().Contains("/TTJets_")
+       ||looper.getCurrentFileName().Contains("/TTTo")
        ||looper.getCurrentFileName().Contains("/WW_")
        ||looper.getCurrentFileName().Contains("/WWTo")
        ||looper.getCurrentFileName().Contains("/WZ_")
@@ -6389,7 +6494,9 @@ bool babyMaker_v2::vetophotonprocess()
         (looper.getCurrentFileName().Contains("/WGTo")
        ||looper.getCurrentFileName().Contains("/WGstar")
        ||looper.getCurrentFileName().Contains("/ZGTo")
-       ||looper.getCurrentFileName().Contains("/TTGamma")
+       ||looper.getCurrentFileName().Contains("/TTGamma_")
+       ||looper.getCurrentFileName().Contains("/TTGJets_")
+       ||looper.getCurrentFileName().Contains("/TGJets_")
        ||looper.getCurrentFileName().Contains("/WWG")
        ||looper.getCurrentFileName().Contains("/WZG")
        )
--- dilepbabymaker/runMetis.py
+++ dilepbabymaker/runMetis.py
@@ -174,16 +174,13 @@ def get_master_list():
             "/WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                     : "wjets_ht1200_madgraph",
             "/WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v3/MINIAODSIM"                      : "wjets_ht2500_madgraph",
             "/ST_s-channel_4f_leptonDecays_TuneCP5_PSweights_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"  : "singletop_schanlep_amcatnlo",
-            #"/ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM" : "singletop_tbrtchanincl_powheg", # Not sure of the status. Too tiny to care.
-            #"/ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"     : "singletop_toptchanincl_powheg", # Not sure of the status. Too tiny to care.
             "/ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"    : "tw_antitopnofullhad_powheg",
             "/ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"        : "tw_topnofullhad_powheg",
+            "/ST_tWll_5f_LO_TuneCP5_PSweights_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v1/MINIAODSIM"                    : "twz_incl_madgraph",
             "/GluGluHToZZTo4L_M125_13TeV_powheg2_JHUGenV7011_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14_ext1-v1/MINIAODSIM"                    : "ggh_hzz4l_powheg",
             "/WpWpJJ_EWK-QCD_TuneCP5_13TeV-madgraph-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                  : "vbsww_madgraph",
             "/WW_DoubleScattering_13TeV-pythia8_TuneCP5/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                      : "ww_dblsct_pythia",
             "/WW_TuneCP5_13TeV-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                                       : "ww_incl_pythia",
-            "/WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"                            : "wz_3lv_amcatnlo",
-            "/WZ_TuneCP5_13TeV-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                                       : "wz_incl_pythia",
             "/WZTo1L1Nu2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"                                 : "wz_lnqq_amcatnlo",
             "/WZTo1L3Nu_13TeV_amcatnloFXFX_madspin_pythia8_v2/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                : "wz_1l3n_amcatnlo",
             "/WZTo2L2Q_13TeV_amcatnloFXFX_madspin_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                    : "wz_2l2q_amcatnlo",
@@ -195,7 +192,7 @@ def get_master_list():
             "/WZTo3LNu_2Jets_MLL-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"                        : "wz_3lv_2jmll50_madgraph",
             "/WZTo3LNu_3Jets_MLL-4to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                     : "wz_3lv_3jmll4_madgraph",
             "/WZTo3LNu_3Jets_MLL-50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"                        : "wz_3lv_3jmll50_madgraph",
-            "/WZTo3LNu_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"                                                  : "wz_3lv_powheg",
+            "/WGToLNuG_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                     : "wg_lvg_madgraph",
             "/ZZTo4L_13TeV_powheg_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                                    : "zz_4l_powheg",
             "/WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                          : "wwz_amcatnlo",
             "/WZZ_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                             : "wzz_amcatnlo",
@@ -206,7 +203,7 @@ def get_master_list():
             "/TTJets_DiLept_TuneCP5_13TeV-madgraphMLM-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                : "ttbar_dilep_mgmlm",
             "/TTGamma_SingleLeptFromT_TuneCP5_PSweights_13TeV_madgraph_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"               : "ttg_1ltop_madgraph",
             "/TTGamma_SingleLeptFromTbar_TuneCP5_PSweights_13TeV_madgraph_pythia8/RunIIFall17MiniAOD-PU2017_94X_mc2017_realistic_v11-v1/MINIAODSIM"                        : "ttg_1ltbr_madgraph",
-            # TTGamma_Dilept
+            "/TTGamma_Dilept_TuneCP5_PSweights_13TeV_madgraph_pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSI"                         : "ttg_dilep_madgraph",
             "/TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                        : "ttw_lv_amcatnlo",
             "/TTWJetsToQQ_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                         : "ttw_qq_amcatnlo",
             "/TTZToLL_M-1to10_TuneCP5_13TeV-amcatnlo-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                 : "ttz_m1to10ll_amcatnlo",
@@ -230,6 +227,13 @@ def get_master_list():
             "/MuonEG/Run2017F-31Mar2018-v1/MINIAOD"                                                                                                                        : "data_Run2017F_em",
             "/VHToWW_M125_13TeV_amcatnloFXFX_madspin_pythia8/PRIVATE_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-ext1-v2/MINIAODSIM"                    : "vh_ww_amcatnlo_private", # Private sample generated by Phil. Restricted to VH->WW but no dilep filter
             "/WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8/PRIVATE_RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                  : "www_amcatnlo_private",   # Private sample generated by Phil. W width not fixed and no dilep filter
+
+            # Not needed
+            # "/ST_t-channel_antitop_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM" : "singletop_tbrtchanincl_powheg", # Not sure of the status. Too tiny to care.
+            # "/ST_t-channel_top_4f_inclusiveDecays_TuneCP5_13TeV-powhegV2-madspin-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"     : "singletop_toptchanincl_powheg", # Not sure of the status. Too tiny to care.
+            # "/WZTo3LNu_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_new_pmx_94X_mc2017_realistic_v14-v1/MINIAODSIM"                            : "wz_3lv_amcatnlo",
+            # "/WZ_TuneCP5_13TeV-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/MINIAODSIM"                                                       : "wz_incl_pythia",
+            # "/WZTo3LNu_13TeV-powheg-pythia8/RunIIFall17MiniAODv2-PU2017_12Apr2018_94X_mc2017_realistic_v14-v2/MINIAODSIM"                                                  : "wz_3lv_powheg",
 
             }
 
