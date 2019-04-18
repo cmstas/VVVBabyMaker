05f1ddbfa12f36fdac2700f678021c03dbb105bb
Philip Chang <sgnoohc@gmail.com>
2019-04-18 12:37:30 -0700

    git hash check script

--- check_git_hash.sh
+++ check_git_hash.sh
@@ -1,18 +1,23 @@
 echo "wwwbabymaker hash"
+git status | head -n 1
 git rev-parse HEAD
 cd CORE
 echo "CORE hash"
+git status | head -n 1
 git rev-parse HEAD
 cd ../
 cd dilepbabymaker/rooutil
+git status | head -n 1
 echo "rooutil hash"
 git rev-parse HEAD
 cd ../
 cd coreutil
 echo "coreutil hash"
+git status | head -n 1
 git rev-parse HEAD
 cd ../
 cd ProjectMetis
 echo "ProjectMetis hash"
+git status | head -n 1
 git rev-parse HEAD
 cd ../../../
