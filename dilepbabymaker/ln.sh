29ae63d98e83a10fb2a592c4a08168d3cfcc4fed
Philip Chang <sgnoohc@gmail.com>
2019-11-03 21:16:13 -0800

    Changed the linking so that the VH samples are now split to various different triboson signals and the rest. this assumes that the split_vh.py from the analysis/ is called on the sample. otherwise the linking will not work properly in placing higgs induced triboson process

--- dilepbabymaker/ln.sh
+++ dilepbabymaker/ln.sh
@@ -33,6 +33,9 @@ VERSION=$1
 
 cd $BASEDIR
 
+rm -rf $1/grouped/
+rm -rf $1/merged/
+
 mkdir -p $1/grouped/
 mkdir -p $1/merged/
 
@@ -56,19 +59,23 @@ mkdir -p sigofficialvh
 mkdir -p sigofficialwww
 mkdir -p sigvh
 mkdir -p sigwww
+mkdir -p sigallwwz
+mkdir -p sigallwzz
+mkdir -p sigallzzz
+mkdir -p vvv
 mkdir -p ttw
 mkdir -p vbs
 
 cd bkg/
 echo linking... bkg
 rm -f *.root
-ln -sf $(ls ../../merged/*.root | grep -v data | grep -v www_ | grep -v ${VHTOSKIPPATTERN} | grep -v ${WGTOSKIP}) .
+ln -sf $(ls ../../merged/*.root | grep -v data | grep -v www_ | grep -v wwz_ | grep -v /wzz_ | grep -v zzz_ | grep -v wh_ww | grep -v zh_ww | grep -v wh_zz | grep -v zh_zz | grep -v /vh_nonbb_ | grep -v ${VHTOSKIPPATTERN} | grep -v ${WGTOSKIP}) .
 cd ..
 cd bkgdata
 echo linking... bkgdata
 rm -f *.root
 # skip wjets_incl because the fake subtractionf or this can often lead to extreme negative subtraction. Not subtracting only leads to more "conservative" estimate of fakes so if anything it will lead to "smaller" sensitivity.
-ln -sf $(ls ../../merged/*.root | grep -v www_ | grep -v ${VHTOSKIPPATTERN} | grep -v ${WGTOSKIP} | grep -v wjets_incl) .
+ln -sf $(ls ../../merged/*.root | grep -v www_ | grep -v wwz_ | grep -v /wzz_ | grep -v zzz_ | grep -v wh_ww | grep -v zh_ww | grep -v wh_zz | grep -v zh_zz | grep -v /vh_nonbb_ | grep -v ${VHTOSKIPPATTERN} | grep -v ${WGTOSKIP} | grep -v wjets_incl) .
 cd ..
 cd bkgnonvbsttw
 echo linking... bkgnonvbsttw
@@ -83,34 +90,64 @@ cd ..
 cd sig
 echo linking... sig
 rm -f *.root
-ln -sf $(ls ../../merged/*.root | grep www | grep -v ${WWWTOSKIPPATTERN}) .
-ln -sf $(ls ../../merged/*.root | grep vh | grep -v ${VHTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /www | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /vh_nonbb_ | grep -v ${VHTOSKIPPATTERN}) .
 cd ..
 cd sigofficial
 echo linking... sigofficial
 rm -f *.root
-ln -sf $(ls ../../merged/*.root | grep www | grep -v ${WWWTOSKIPPATTERN}) .
-ln -sf $(ls ../../merged/*.root | grep vh | grep -v ${VHTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /www | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /vh_nonbb_ | grep -v ${VHTOSKIPPATTERN}) .
 cd ..
 cd sigofficialvh
 echo linking... sigofficialvh
 rm -f *.root
-ln -sf $(ls ../../merged/*.root | grep vh | grep -v ${VHTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /vh_nonbb_ | grep -v ${VHTOSKIPPATTERN}) .
 cd ..
 cd sigofficialwww
 echo linking... sigofficialwww
 rm -f *.root
-ln -sf $(ls ../../merged/*.root | grep www | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /www | grep -v ${WWWTOSKIPPATTERN}) .
 cd ..
 cd sigvh
 echo linking... sigvh
 rm -f *.root
-ln -sf $(ls ../../merged/*.root | grep vh | grep -v ${VHTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /vh_nonbb_ | grep -v ${VHTOSKIPPATTERN}) .
 cd ..
 cd sigwww
 echo linking... sigwww
 rm -f *.root
-ln -sf $(ls ../../merged/*.root | grep www | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /www | grep -v ${WWWTOSKIPPATTERN}) .
+cd ..
+cd sigallwwz
+echo linking... sigallwwz
+rm -f *.root
+ln -sf $(ls ../../merged/*.root | grep /wwz_ | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep zh_ww | grep -v ${WWWTOSKIPPATTERN}) .
+cd ..
+cd sigallwzz
+echo linking... sigallwzz
+rm -f *.root
+ln -sf $(ls ../../merged/*.root | grep /wzz_ | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep wh_zz | grep -v ${WWWTOSKIPPATTERN}) .
+cd ..
+cd sigallzzz
+echo linking... sigallzzz
+rm -f *.root
+ln -sf $(ls ../../merged/*.root | grep /zzz_ | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep zh_zz | grep -v ${WWWTOSKIPPATTERN}) .
+cd ..
+cd vvv
+echo linking... vvv
+rm -f *.root
+ln -sf $(ls ../../merged/*.root | grep /zzz_ | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep zh_zz | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /wzz_ | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep wh_zz | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /wwz_ | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep zh_ww | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep /www | grep -v ${WWWTOSKIPPATTERN}) .
+ln -sf $(ls ../../merged/*.root | grep wh_ww | grep -v ${VHTOSKIPPATTERN}) .
 cd ..
 cd ttw
 echo linking... ttw
